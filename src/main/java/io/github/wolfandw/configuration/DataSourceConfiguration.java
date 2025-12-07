cd78f269699246f9000a1cecece3abec2269d606
Andrey Volkov <wolf.and.w@yandex.ru>
2025-12-07 21:06:38 +0300

    Настройка базы данных

---
+++ src/main/java/io/github/wolfandw/configuration/DataSourceConfiguration.java
@@ -0,0 +1,48 @@
+package io.github.wolfandw.configuration;
+
+import org.h2.Driver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+    @Bean
+    public DataSource dataSource(
+            // Настройки соединения возьмём из Environment
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password
+    ) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Driver.class.getName());
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    // После инициализации контекста выполняем наполнение схемы базы данных
+    @EventListener
+    public void populate(ContextRefreshedEvent event) {
+        DataSource dataSource = event.getApplicationContext().getBean(DataSource.class);
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql")); // Файл должен находиться в ресурсах
+        populator.execute(dataSource);
+    }
+}
